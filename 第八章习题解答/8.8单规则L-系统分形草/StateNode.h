3ef2d0a1eb498fc9c0bf8d12a54a060d1c354cb9
Tast <TastSong@Gmail.com>
2019-07-02 22:05:20 +0800

    单规则L-系统分形草
    8.8单规则L-系统分形草

---
+++ 第八章习题解答/8.8单规则L-系统分形草/StateNode.h
@@ -0,0 +1,22 @@
+// StateNode.h: interface for the StateNode class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_STATENODE_H__B1961E0B_90C0_4464_A3A2_0DB8EF4EB931__INCLUDED_)
+#define AFX_STATENODE_H__B1961E0B_90C0_4464_A3A2_0DB8EF4EB931__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CStateNode  //Ω·µ„¿‡
+{
+public:
+	CStateNode();
+	virtual ~CStateNode();
+	double x;
+	double y;
+	double afa;
+};
+
+#endif // !defined(AFX_STATENODE_H__B1961E0B_90C0_4464_A3A2_0DB8EF4EB931__INCLUDED_)
---
+++ 第八章习题解答/8.8单规则L-系统分形草/TestView.h
@@ -0,0 +1,73 @@
+// TestView.h : interface of the CTestView class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_TESTVIEW_H__7B09737C_A6B6_4402_B2B8_087738721CF6__INCLUDED_)
+#define AFX_TESTVIEW_H__7B09737C_A6B6_4402_B2B8_087738721CF6__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+#include "StateNode.h"//包含结点头文件
+
+class CTestView : public CView
+{
+protected: // create from serialization only
+	CTestView();
+	DECLARE_DYNCREATE(CTestView)
+
+// Attributes
+public:
+	CTestDoc* GetDocument();
+
+// Operations
+public:
+	void GetMaxX();//获得屏幕最大x值函数
+    void GetMaxY();//获得屏幕最大y值函数
+	void GrammarModel(int);//文法模型
+	void GrammarExplanation(double,double);//文法解释
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CTestView)
+	public:
+	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CTestView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+	int MaxX,MaxY;//屏幕最大值
+	double ax,ay;//起点坐标
+	CString Axiom,Rule,NewRule,NewRuleTemp;//文法模型参数
+	CStateNode Stack[1024];//堆栈数组
+	int	StackPushPos;//堆栈数组元素位置
+// Generated message map functions
+protected:
+	//{{AFX_MSG(CTestView)
+	afx_msg void OnMENUGrass();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // debug version in TestView.cpp
+inline CTestDoc* CTestView::GetDocument()
+   { return (CTestDoc*)m_pDocument; }
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_TESTVIEW_H__7B09737C_A6B6_4402_B2B8_087738721CF6__INCLUDED_)
